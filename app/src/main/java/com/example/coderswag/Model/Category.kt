3855bf7540568c899a90fc6f347209ccce6670bf
Thavakumaran Jeyananthan <thavakumaran_jeyananthan@astro.com.my>
2019-03-13 14:59:29 +0800

    lesson-01

---
+++ app/src/main/java/com/example/coderswag/Model/Category.kt
@@ -0,0 +1,9 @@
+package com.example.coderswag.Model
+
+import android.support.v7.widget.DialogTitle
+
+class Category (
+    val title: String,
+    val image: String
+) {
+}
---
+++ app/src/main/java/com/example/coderswag/Model/Product.kt
@@ -0,0 +1,8 @@
+package com.example.coderswag.Model
+
+class Product(
+    val title: String,
+    val price: String,
+    val image: String
+) {
+}
